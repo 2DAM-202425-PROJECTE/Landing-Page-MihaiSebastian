45b192bb318ba1333fc40ed189258b83c8422f6b
MihaiSebastian <mburghele@iesebre.com>
2024-09-24 20:00:23 +0200

    Sync

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./components/**/*.{html,js}',
+    './pages/**/*.{html,js}',
+    './index.html',],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
